5fd4146024ee79ea5ab1519021856724d4c15589
Evaldo Araujo <evaldo.araujo@yahoo.com.br>
2020-06-29 13:56:06 -0300

    Chess layer and printing the board

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -6,6 +6,37 @@ public class Position
 	private int column;
 	
 	//criar construtor
+	public Position(int row, int column) 
+	{
+		this.row = row;
+		this.column = column;
+	}
+
+	public int getRow() 
+	{
+		return row;
+	}
+
+	public void setRow(int row)
+	{
+		this.row = row;
+	}
+
+	public int getColumn()
+	{
+		return column;
+	}
+
+	public void setColumn(int column) 
+	{
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() 
+	{
+		return row + ", " + column;
+	}
 	
 	
 }
