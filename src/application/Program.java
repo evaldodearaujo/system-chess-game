2a36090a74d5d532ce669311c9157c04eae95924
Evaldo Araujo <evaldo.araujo@yahoo.com.br>
2020-06-29 12:54:21 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,11 @@
 package application;
 
-public class Program {
+public class Program 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
