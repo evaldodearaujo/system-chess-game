add4de3a9398e9b0202230f8c223df842baf246a
Evaldo Araujo <evaldo.araujo@yahoo.com.br>
2020-06-29 16:14:25 -0300

    Little improvement in board printing

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,7 +8,7 @@ public class Program
 	public static void main(String[] args) 
 	{
 		ChessMatch chessMatch = new ChessMatch();
-		UI.printBoad(chessMatch.getPieces());
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
