68534f72f1c4040c1761b494f0f90c9c111688ba
Evaldo Araujo <evaldo.araujo@yahoo.com.br>
2020-06-29 21:23:31 -0300

    Implementing possible moves of Rock

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -34,6 +34,7 @@ public class ChessMatch
 		Position source = sourcePosition.toPostiont();
 		Position target = targetPosition.toPostiont();
 		validateSourcePosition(source);
+		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
 		return (ChessPiece) capturedPiece;
 	}
@@ -59,6 +60,13 @@ public class ChessMatch
 		}
 	}
 	
+	private void validateTargetPosition(Position source, Position target)
+	{
+		if(!board.piece(source).possibleMove(target))
+		{
+			throw new ChessException("The chosen piece can't move to target position");
+		}
+	}
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece)
 	{
